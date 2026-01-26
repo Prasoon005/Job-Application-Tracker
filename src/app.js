27368b68d5244cfc4f4e96b93f216e15a1c05e5f
Prasoon005 <katiyarprasoon003@gmail.com>
2026-01-26 23:16:04 +0530

    created authoController users_model authRoutes and modified package.json implemented secure authentication using bcrypt for password hashing and JWT for authorization

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-
+const authRoutes = require("./routes/authRoutes");
 const app = express();
 
 // middlewares
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Job Tracker API is running ...");
