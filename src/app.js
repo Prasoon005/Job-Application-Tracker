6652ac2517cfec6f247b15452d31704b3e1a16db
Prasoon005 <katiyarprasoon003@gmail.com>
2026-01-28 00:25:52 +0530

    adding the job route in app.js

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const app = express();
+const jobRoutes = require("./routes/jobRoutes.js");
+
 
 // middlewares
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/jobs", jobRoutes);
 app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
